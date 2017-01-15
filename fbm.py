e987ba285acd125028af8e1f72e51ab58013046a
Dibya Das <dibyadascool@gmail.com>
2017-01-15 20:24:41 +0530

    Made changes to store and retrieve the login details properly

--- fbm.py
+++ fbm.py
@@ -6,9 +6,19 @@ login_details = os.path.expanduser('~') + '/.login'
 
 try:
 	file = open(login_details,'r')
-	email = 
-except:
+	email = file.readline()
+	pwd = file.readline()
+	me = fbchat.Client(email,pwd)
+	file.close()
+except FileNotFoundError:
 	email = input("Please enter your email-id:-")
-	pwd = input("Please enter your password")
+	pwd = input("Please enter your password:-")
+	file = open(login_details,'w')
+	file.writelines(email)
+	file.writelines("\n")
+	file.writelines(pwd)
+	me = fbchat.Client(email,pwd)
+	file.close()
 
-me = fbchat.Client(email,pwd)
+
+
